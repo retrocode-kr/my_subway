104b83c0b8a5a6c6c16affad53521c93d26aa329
sypark909@gmail.com <sypark@sypark-miniPC.localdomain>
2026-09-02 10:28:55 +0900

    feat: Initial commit for HACS component

---
+++ custom_components/my_subway/__init__.py
@@ -0,0 +1,9 @@
+"""The My Subway Arrival integration."""
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+DOMAIN = "my_subway"
+
+def setup(hass, config):
+    """Set up the component."""
+    return True
---
+++ custom_components/my_subway/sensor.py
@@ -0,0 +1,34 @@
+import requests
+from datetime import timedelta
+from homeassistant.helpers.entity import Entity
+
+SCAN_INTERVAL = timedelta(seconds=60) # 60초마다 API 호출
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the subway sensor platform."""
+    api_key = config.get("api_key")
+    station = config.get("station", "부평")
+    add_entities([SubwaySensor(api_key, station)], True)
+
+class SubwaySensor(Entity):
+    def __init__(self, api_key, station):
+        self._api_key = api_key
+        self._station = station
+        self._state = None
+
+    @property
+    def name(self):
+        return f"지하철 도착 정보 ({self._station})"
+
+    @property
+    def state(self):
+        return self._state
+
+    def update(self):
+        url = f"http://swopenapi.seoul.go.kr/api/subway/744357696c7073793739527250724d/json/realtimeStationArrival/1/4/%EB%B6%80%EA%B0%9C/"
+        try:
+            response = requests.get(url, timeout=10)
+            data = response.json()
+            self._state = data['realtimeArrivalList'][0]['arvlMsg2']
+        except Exception:
+            self._state = "Error"
