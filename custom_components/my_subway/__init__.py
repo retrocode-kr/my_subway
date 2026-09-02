42949608c19fe46659cd18b6ab26075ae31c4efb
sypark909@gmail.com <sypark@sypark-miniPC.localdomain>
2026-09-02 13:33:56 +0900

    fix: update sensor.py structure

--- custom_components/my_subway/__init__.py
+++ custom_components/my_subway/__init__.py
@@ -2,6 +2,7 @@
 import logging
 
 _LOGGER = logging.getLogger(__name__)
+_LOGGER.warning("MY_SUBWAY V2 LOADED - unique_id=%s", self._attr_unique_id)
 DOMAIN = "my_subway"
 
 def setup(hass, config):
